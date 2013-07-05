577b693875e21deb8ac2109ac3e00f9eb9221fe7
Andy Kent <andy.kent@me.com>
2013-07-05 21:53:41 +0100

    Opps! TriggerRunner shouldn't throw when args are given

--- lib/orca/trigger_runner.rb
+++ lib/orca/trigger_runner.rb
@@ -1,16 +1,16 @@
 class Orca::TriggerRunner
   def initialize(node, action_ref_with_args)
     @node = node
-    @action_ref, args = *parse_action_ref(action_ref_with_args)
+    @action_ref, @args = *parse_action_ref(action_ref_with_args)
     @log = Orca::Logger.new(@node, @action_ref)
   end
 
   def execute(_)
-    Orca::ExecutionContext.new(@node, @log).trigger(@action_ref, *args)
+    Orca::ExecutionContext.new(@node, @log).trigger(@action_ref, *@args)
   end
 
   def demonstrate(_)
-    Orca::MockExecutionContext.new(@node, @log).trigger(@action_ref, *args)
+    Orca::MockExecutionContext.new(@node, @log).trigger(@action_ref, *@args)
   end
 
   private
@@ -20,6 +20,6 @@ class Orca::TriggerRunner
     return [action_ref_with_args,[]] unless matches
     action_ref = matches[1]
     args = matches[3].split(',').map(&:strip)
-    throw [action_ref, args]
+    [action_ref, args]
   end
 end
